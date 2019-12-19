35554cf29e10937583d2c0f1688c9f6427442665
Victor Nordstrand <v_nordstrand@outlook.com>
2019-12-19 18:30:57 +0100

    added BaseEntity

--- pet-clinic-data/src/main/java/dk/apendo/petclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/dk/apendo/petclinic/model/Pet.java
@@ -2,7 +2,7 @@ package dk.apendo.petclinic.model;
 
 import java.time.LocalDate;
 
-public class Pet {
+public class Pet extends BaseEntity {
     private LocalDate birthDate;
     private PetType petType;
     private Owner owner;
