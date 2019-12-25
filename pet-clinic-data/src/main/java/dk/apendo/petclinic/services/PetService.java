d3fbccf950a152916ac7ba3a516571427c6f8e39
Victor Nordstrand <v_nordstrand@outlook.com>
2019-12-25 14:41:10 +0100

    refactoring to common crud interface

--- pet-clinic-data/src/main/java/dk/apendo/petclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/dk/apendo/petclinic/services/PetService.java
@@ -1,14 +1,7 @@
 package dk.apendo.petclinic.services;
 
 import dk.apendo.petclinic.model.Pet;
-import java.util.Set;
 
-public interface PetService {
-
-    Pet findById(Long id);
-
-    Pet save(Pet owner);
-
-    Set<Pet> findAll();
+public interface PetService extends CrudService<Pet, Long> {
 
 }
--- pet-clinic-data/src/main/java/dk/apendo/petclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/dk/apendo/petclinic/services/VetService.java
@@ -2,14 +2,6 @@ package dk.apendo.petclinic.services;
 
 import dk.apendo.petclinic.model.Vet;
 
-import java.util.Set;
-
-public interface VetService {
-
-    Vet findById(Long id);
-
-    Vet save(Vet owner);
-
-    Set<Vet> findAll();
+public interface VetService extends CrudService<Vet, Long> {
 
 }
