859941886ca34344b96e5f79fdc29932736c7320
Victor Nordstrand <v_nordstrand@outlook.com>
2020-01-13 23:18:37 +0100

    added visit entity

--- pet-clinic-data/src/main/java/dk/apendo/petclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/dk/apendo/petclinic/model/Owner.java
@@ -1,5 +1,16 @@
 package dk.apendo.petclinic.model;
 
+import java.util.Set;
+
 public class Owner extends Person {
 
+    private Set<Pet> pet;
+
+    public Set<Pet> getPet() {
+        return pet;
+    }
+
+    public void setPet(Set<Pet> pet) {
+        this.pet = pet;
+    }
 }
