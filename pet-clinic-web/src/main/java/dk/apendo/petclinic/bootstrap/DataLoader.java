c5c3c9564c623c5089900c0209baae11fa411e8c
Victor Nordstrand <v_nordstrand@outlook.com>
2020-01-25 14:20:15 +0100

    Added PetType to DataLoader

--- pet-clinic-web/src/main/java/dk/apendo/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/dk/apendo/petclinic/bootstrap/DataLoader.java
@@ -1,9 +1,11 @@
 package dk.apendo.petclinic.bootstrap;
 
 import dk.apendo.petclinic.model.Owner;
+import dk.apendo.petclinic.model.PetType;
 import dk.apendo.petclinic.model.Vet;
 import dk.apendo.petclinic.services.OwnerService;
 import dk.apendo.petclinic.services.PetService;
+import dk.apendo.petclinic.services.PetTypeService;
 import dk.apendo.petclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -14,15 +16,27 @@ public class DataLoader implements CommandLineRunner {
     private final OwnerService ownerService;
     private final VetService vetService;
     private final PetService petService;
+    private final PetTypeService petTypeService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService, PetService petService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetService petService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
         this.petService = petService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) throws Exception {
+
+        PetType dog = new PetType();
+        dog.setName("Dog");
+
+        PetType savedDogType = petTypeService.save(dog);
+
+        PetType cat = new PetType();
+        cat.setName("Cat");
+
+        PetType savedCatType = petTypeService.save(cat);
 
         Owner owner1 = new Owner();
         owner1.setFirstName("Michael");
