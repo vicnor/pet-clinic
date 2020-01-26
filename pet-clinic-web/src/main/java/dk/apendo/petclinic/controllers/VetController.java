ba3ab12adfcc04e09e7f1bfaeef0ec2decb4c2b5
Victor Nordstrand <victor.nordstrand@apendo.dk>
2020-01-26 23:16:06 +0100

    fixed broken link

--- pet-clinic-web/src/main/java/dk/apendo/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/dk/apendo/petclinic/controllers/VetController.java
@@ -6,6 +6,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/vets")
 public class VetController {
 
     private final VetService vetService;
@@ -14,7 +15,7 @@ public class VetController {
         this.vetService = vetService;
     }
 
-    @RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
+    @RequestMapping({"", "/", "/index", "/index.html"})
     public String listVets(Model model) {
         model.addAttribute("vets", vetService.findAll());
         return "vets/index";
